f9659388454095d7cce0bfe8b59170d03522a27b
alisawiali <alisawiali70@gmail.com>
2024-03-03 19:19:49 +0100

    Update  register und login mit hash bcrypt

--- api/src/controller/auth.js
+++ api/src/controller/auth.js
@@ -1,14 +1,46 @@
 import User from "../modals/User.js";
+/* Node.js Bibliothek 
+ bcryptjs bietet automatisch Salting an,
+ was bedeutet, dass jedem Passwort vor dem Hashing eine
+  zufällige Zeichenfolge (Salz) hinzugefügt wird. Dadurch
+   wird verhindert, dass identische Passwörter denselben
+    Hash-Wert erzeugen, was die Sicherheit erhöht
+*/
+import bcrypt from "bcryptjs";
 
+//  Fehlerbehandlung ======>
+import { createErrro } from "../utils/error.js";
+
+//  Register ======>
 export const register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
-      password: req.body.password,
+      password: hash,
     });
     await newUser.save();
     res.status(200).send("user has been created");
+  } catch (error) {
+    next(error);
+  }
+};
+
+//  Login ======>  compare  يقارن
+export const login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) return next(createErrro(404, "user not found!"));
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!isPasswordCorrect)
+      return next(createErrro(400, "Password incorrect or username!"));
+    res.status(200).json(user);
   } catch (error) {
     next(error);
   }
--- api/src/modals/User.js
+++ api/src/modals/User.js
@@ -13,7 +13,7 @@ const userSschema = new Schema(
       unique: true,
     },
     password: {
-      type: Number,
+      type: String,
       required: true,
     },
     isAdmin: {
