321ecf07aebe0268761afa5a5fc824d83faf2601
alisawiali <alisawiali70@gmail.com>
2024-03-06 14:49:58 +0100

    Update yarn und erstellt useFetch

---
+++ frontend/src/hooks/useFetch.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await axios.get(url);
+        setData(res.data);
+      } catch (error) {
+        setError(error);
+      }
+      setLoading(false);
+    };
+    fetchData(); // Aufruf von fetchData außerhalb des try-catch-Blocks
+  }, [url]); // Leere Abhängigkeitsliste für einmaliges Ausführen beim ersten Rendern
+
+  // -------------------------------
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(url);
+      setData(res.data);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  return { data, loading, error, reFetch };
+};
+
+export default useFetch;
